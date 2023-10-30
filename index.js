f6a40285ee797dfbd2e5bc86738c6378f876918a
Boi <cambodianjesuss@gmail.com>
2023-10-30 15:18:09 -0700

    sneding response of data being emmited and picked up by query service

--- index.js
+++ index.js
@@ -10,6 +10,37 @@ app.use(cors());
  * Object that recieves an event, then stores it data to send only one request
  */
 
+/**
+ * Data Shape
+ * postId: {
+ *  id: postId,
+ * title: 'post title'
+ * comments: [
+ *    { id: commentId, content: 'comment!'}
+ *  ]
+ * }
+ */
+const posts = {
+  'jfdie344test': {
+    id: 'jfdie344test',
+    title: 'This is a test of the query service response',
+    comments: [
+      {
+        id: 'commentIdtest123',
+        content: 'This is a test comment for query'
+      },
+      {
+        id: 'commentIdtest456',
+        content: 'Another test comment for query'
+      }
+    ]
+  }
+}
+
+app.get('/posts', (req, res)=>{
+  res.send(posts);
+})
+
 app.post('/events', (req, res)=>{
   console.log("Event Type Recieved in Query:", req.body.type);
   res.send({Status: "OK"});
