09d8c40803d9a3d4e4ffda4f55ae176089c95cad
Boi <cambodianjesuss@gmail.com>
2023-10-30 14:13:41 -0700

    setting up the data structure

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
+
+/**
+ * Object that recieves an event, then stores it data to send only one request
+ */
 
 app.post('/events', (req, res)=>{
-  console.log(req.body.type)
+  console.log("Event Type Recieved in Query:", req.body.type);
+  res.send({Status: "OK"});
 });
 
 app.listen(4002, ()=>{
