d6581ded379227b0c8119b7db8079ccc7d6e79f3
Boi <cambodianjesuss@gmail.com>
2023-10-30 11:19:17 -0700

    setting up evenet recieved

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/events', (req, res)=>{
+  console.log(req.body)
+});
 
 app.listen(4002, ()=>{
   console.log('Query Service on 4002')
